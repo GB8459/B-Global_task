ea2a003cbe06d7534b19437f8f5e4b44229e655c
sardorDt101 <sardor.k@datatruck.io>
2025-11-01 16:31:35 +0500

    updated bk module

--- src/bk.py
+++ src/bk.py
@@ -27,7 +27,7 @@ class BlastForum:
     def extract_text(self, contents: Response) -> str:
         return contents.text;
 
-    def _get_links(self, pattern: str, page_html: str):
+    def _get_groups(self, pattern: str, page_html: str):
         try:
             return self._text_handler.search_pattern(pattern, page_html)
         except Exception as e:
@@ -41,19 +41,19 @@ class BlastForum:
 
     def get_heading_link(self, heading:str, page_html: str):
         pattern = fr'<a [\s\w\W\"]*href=\"(.*?)\"[\"\s\W\w]+>{heading}</a>'
-        return self._get_links(pattern, page_html);
+        return self._get_groups(pattern, page_html);
 
     def get_topic_links(self, page_html: str ):
         pattern = r'<a href=\"(/threads/\d+/)\" class='
-        return self._get_links(pattern, page_html)[0:5];
+        return self._get_groups(pattern, page_html)[0:5];
 
     def get_last_page_link(self, thread_link: str, page_html: str):
         pattern = fr'(?<=pageNav-page \"><a href=\"){thread_link}page-\d+(?=\">\d+</a>)'
-        return self._get_links(pattern, page_html);
+        return self._get_groups(pattern, page_html);
 
     # def get_messages_with_quotes(self, page_html: str):
     #     pattern = r'<div class=\"bbWrapper\">[</\w\s\W\">\n]*?te>(.*?)</div>\n'
-    #     return self._get_links(pattern, page_html);
+    #     return self._get_groups(pattern, page_html);
 
     def remove_quotes(self, page_html: str):
         pattern = r'<blockquote[\"\w\W\s<>]*?</blockquote>'
@@ -61,20 +61,20 @@ class BlastForum:
 
     def get_messages_with_no_quotes(self, page_html: str):
         pattern = r'<div class=\"bbWrapper\">(.*?)</div>\n'
-        return self._get_links(pattern, page_html);
+        return self._get_groups(pattern, page_html);
 
     def get_articles(self, page_html: str):
         pattern = r'<div [\w\s\W\"=]*?>[\n\s]+(<article[\s\w\W\"-<>]*?</article>)\n+\s+</div>'
-        return self._get_links(pattern, page_html);
+        return self._get_groups(pattern, page_html);
 
     def get_author_and_time(self, page_html: str):
         pattern = r'data-lb-caption-desc=\"(.*?)\">'
-        return self._get_links(pattern, page_html);
+        return self._get_groups(pattern, page_html);
 
     def get_article_title(self, page_html: str):
         pattern = r'<a href=\"/threads/\d+/\"[\w\s\W\"<>\n/]*?>(.*?)</a>\n\t\t\t</div>'
-        return self._get_links(pattern, page_html);
+        return self._get_groups(pattern, page_html);
 
     def get_words(self, text: str):
         pattern = r'\b(?!и|с|ли|или|в|ну|не|по|уже\b)\w+\b'
-        return self._get_links(pattern, text);
+        return self._get_groups(pattern, text);
